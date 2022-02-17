80dc90aef549b2c5336e14dec8f47bb98fef36b8
Jody Prinsloo <jprinsloo@my.bcit.ca>
2022-02-16 19:55:08 -0800

    Added Skills + Tools Table

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -10,7 +10,7 @@ const About = () => {
     return (
         <div className="a">
             <div className="a-left">
-                <div className="a-card bg" style={{backgroundColor: darkMode ? "#313030" : "#DEE1E3"}}></div>
+                {/* <div className="a-card bg" style={{backgroundColor: darkMode ? "#313030" : "#DEE1E3"}}></div> */}
                 <div className="a-card">
                     <img src="https://placekitten.com/500/500" alt="" className="a-img" />
                 </div>
@@ -24,13 +24,73 @@ const About = () => {
                 Sequi dolore ex facere eum! Tempora odit similique unde iure mollitia voluptatum, 
                 ut dolore enim cum saepe numquam porro repellendus nihil aliquam.
                 </p>
-                <div className="a-award">
-                     {/*This is a placeholder image for now */}
-                    <img src="https://placekitten.com/250/250" alt="" className="a-award-img" />
-                    <div className="a-award-texts">
-                        <h4 className="a-award-title" style={{color: darkMode ? "#00FFD1" : "#666666"}}>Award #1</h4>
-                        <p className="a-award-desc">This is a short description of an award I have received</p>
-                    </div>
+                <div className="skillsToolsCont">
+                     <div className="SkillsCont">
+                         <div className="SkillsTitleBar">
+                             <h2 className="SkillsHeader">Soft Skills</h2>
+                         </div>
+                         <div className="BulletSkill">
+                            <img src="https://placekitten.com/50/50" alt="" className="skillImg" />
+                            <p className="SkillName">Communication</p>
+                         </div>
+                         <div className="BulletSkill">
+                            <img src="https://placekitten.com/50/50" alt="" className="skillImg" />
+                            <p className="SkillName">Organization</p>
+                         </div>
+                         <div className="BulletSkill">
+                            <img src="https://placekitten.com/50/50" alt="" className="skillImg" />
+                            <p className="SkillName">Team Oriented</p>
+                         </div>
+                         <div className="BulletSkill">
+                            <img src="https://placekitten.com/50/50" alt="" className="skillImg" />
+                            <p className="SkillName">Adaptive</p>
+                         </div>
+                         <div className="BulletSkill">
+                            <img src="https://placekitten.com/50/50" alt="" className="skillImg" />
+                            <p className="SkillName">Eye For Detail</p>
+                         </div>
+                         <div className="BulletSkill">
+                            <img src="https://placekitten.com/50/50" alt="" className="skillImg" />
+                            <p className="SkillName">Punctual</p>
+                         </div>
+                         <div className="BulletSkill">
+                            <img src="https://placekitten.com/50/50" alt="" className="skillImg" />
+                            <p className="SkillName">Works Well Under Pressure</p>
+                         </div>
+                     </div>
+                     <div className="ToolsCont">
+                         <div className="ToolsTitleBar">
+                            <h2 className="SkillsHeader">Toolset</h2>
+                         </div>
+                         <div className="BulletTool">
+                             <img src="https://placekitten.com/50/50" alt="" className="toolImg" />
+                             <p className="ToolName">Adobe Photoshop</p>
+                         </div>
+                         <div className="BulletTool">
+                             <img src="https://placekitten.com/50/50" alt="" className="toolImg" />
+                             <p className="ToolName">Adobe Illustrator</p>
+                         </div>
+                         <div className="BulletTool">
+                             <img src="https://placekitten.com/50/50" alt="" className="toolImg" />
+                             <p className="ToolName">Adobe After Effects</p>
+                         </div>
+                         <div className="BulletTool">
+                             <img src="https://placekitten.com/50/50" alt="" className="toolImg" />
+                             <p className="ToolName">Adobe InDesign</p>
+                         </div>
+                         <div className="BulletTool">
+                             <img src="https://placekitten.com/50/50" alt="" className="toolImg" />
+                             <p className="ToolName">Figma</p>
+                         </div>
+                         <div className="BulletTool">
+                             <img src="https://placekitten.com/50/50" alt="" className="toolImg" />
+                             <p className="ToolName">HTML5, CSS, Javascript</p>
+                         </div>
+                         <div className="BulletTool">
+                             <img src="https://placekitten.com/50/50" alt="" className="toolImg" />
+                             <p className="ToolName">MySQL, jQuery</p>
+                         </div>
+                     </div>
                 </div>
             </div>
         </div>
