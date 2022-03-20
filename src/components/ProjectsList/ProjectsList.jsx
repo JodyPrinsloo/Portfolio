c591697277c8e08fc34c57a6cfd8fed2dee6f71b
Jody Prinsloo <jprinsloo@my.bcit.ca>
2022-03-20 11:26:02 -0700

    Added sections for UI/UX and Graphic Design

--- src/components/ProjectsList/ProjectsList.jsx
+++ src/components/ProjectsList/ProjectsList.jsx
@@ -4,6 +4,16 @@ import {projects_img} from "../../data";
 import { useContext } from "react";
 import { ThemeContext } from "../../context";
 
+//UI Image Imports
+import BBMockup from "../../img/Buzzy-Bee-Mockups.png";
+import ScoutMockup from "../../img/Scout.png";
+import CraigslistMockup from "../../img/Craigslist-Mockup.png";
+
+//Graphic Design Image Imports
+import DrStrange from "../../img/Dr-Strange-Dribbble.png";
+import LeadTheWay from "../../img/Lead-The-Way-Dribbble.png";
+import Gihun from "../../img/Gi-Hun-Dribbble.png";
+
 const ProjectsList = () => {
 
   const theme = useContext(ThemeContext);
@@ -21,6 +31,20 @@ const ProjectsList = () => {
           <Projects key={item.id} img={item.img} link={item.link}/>
           ))}
         </div>
+        {/* UI UX Section */}
+        <h1 className="pl-titles" style={{color: darkMode ? "#00FFD1" : "#006654"}}>UI/UX Work</h1>
+          <div className="uiux">
+              <Projects img={BBMockup} link="https://dribbble.com/shots/17443792-Mobile-Application-UI-UX-Design-Buzzy-Bee"/>
+              <Projects img={ScoutMockup}/>
+              <Projects img={CraigslistMockup} link="https://dribbble.com/shots/17443718-UI-UX-Craigslist-Redesign-Web-Mobile"/>
+          </div>
+        {/* Graphic Design Section */}
+        <h1 className="pl-titles" style={{color: darkMode ? "#00FFD1" : "#006654"}}>Graphic Design Work</h1>
+          <div className="uiux">
+              <Projects img={DrStrange} link="https://dribbble.com/shots/17443737-Photo-Manipulation-Dr-Strange"/>
+              <Projects img={LeadTheWay} link="https://dribbble.com/shots/17443745-Photo-Manipulation-Unknown-Territory"/>
+              <Projects img={Gihun} link="https://dribbble.com/shots/17443731-Digital-Art-Squid-Game-Drawing"/>
+          </div>
     </div>
   )
 }
